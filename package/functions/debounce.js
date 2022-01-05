0d156ab446776fc7b4d2fcbbe3ac22d47a0b36b6
TY-Subin-S <subin.s@testyantra.in>
2022-01-05 19:26:18 +0530

    debounce and throttle

---
+++ package/functions/debounce.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param {Function} func debounce function
+ * @param {Number} wait settimeout time
+ * @param {Boolean} immediate boolean condition
+ * @returns
+ */
+function debounce(func, wait, immediate) {
+  var timeout;
+
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+}
+
+module.exports = debounce;
